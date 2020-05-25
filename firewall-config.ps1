f5793196ea5a1006c2b728cc71266594c74d014e
siavashy88 <61756513+siavashy88@users.noreply.github.com>
2020-05-25 13:11:20 -0400

    Update firewall-config.ps1

--- firewall-config.ps1
+++ firewall-config.ps1
@@ -131,25 +131,25 @@ if ($fwcfg.pfsense.interfaces.wan.gateway -eq $primarywangateway) {
 }
 
 if ($fwcfg.pfsense.interfaces.lan.if -eq $values.laninterface) {
-    Write-Host "Primary LAN interface is already configured"  -ForegroundColor Green
+    Write-Host "LAN interface is already configured"  -ForegroundColor Green
 } else {
-    Write-Host "Primary LAN interface will change to $($values.laninterface)"  -ForegroundColor Red
+    Write-Host "LAN interface will change to $($values.laninterface)"  -ForegroundColor Red
     $laninterfaceconfig = 1
     $configflag = 1
 }
 
 if ($fwcfg.pfsense.interfaces.lan.ipaddr -eq $lanipaddr) {
-    Write-Host "Primary LAN IP address is already configured"  -ForegroundColor Green
+    Write-Host "LAN IP address is already configured"  -ForegroundColor Green
 } else {
-    Write-Host "Primary LAN IP address will change to $lanipaddr"  -ForegroundColor Red
+    Write-Host "LAN IP address will change to $lanipaddr"  -ForegroundColor Red
     $lanipaddrconfig = 1
     $configflag = 1
 }
 
 if ($fwcfg.pfsense.interfaces.lan.subnet -eq $lansubnet) {
-    Write-Host "Primary LAN subnet is already configured"  -ForegroundColor Green
+    Write-Host "LAN subnet is already configured"  -ForegroundColor Green
 } else {
-    Write-Host "Primary LAN subnet will change to $lansubnet"  -ForegroundColor Red
+    Write-Host "LAN subnet will change to $lansubnet"  -ForegroundColor Red
     $lansubnetconfig = 1
     $configflag = 1
 }
